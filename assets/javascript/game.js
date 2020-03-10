59423089ecb80b426fd98acd4c1086d2c6bb95bb
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-03-09 19:30:45 -0600

    added variables and initial js on key up event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,15 @@
+//Variables
 var myWords = ["cell", "chiaotzu", "frieza", "gohan", "goku", "krillin", "majin buu", "piccolo", "tien", "trunks", "vegeta", "yamcha"];
+var wins = 0;
+var winsText = document.getElementById("wins-text");
+var random = document.getElementById("random-computer");
+var guessesLeft = document.getElementById("guesses-left");
+
+//start code
+document.onkeyup = function(event) {
+var userInput = event.key;
+var random = myWords[Math.floor(Math.random() * myWords.length)];
+
+
+}
+

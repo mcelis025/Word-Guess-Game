62400331a676184ff1519e9da20cbe3798fe65a7
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-03-14 23:20:54 -0600

    variables added to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,5 @@
-//Variables
+// Variables
 var myWords = ["cell", "chiaotzu", "frieza", "gohan", "goku", "krillin", "majin buu", "piccolo", "tien", "trunks", "vegeta", "yamcha"];
 var wins = 0;
-var blankWord = [];
-var userInput = [];
-
-var winsText = document.getElementById("wins-text");
-var random = document.getElementById("random-computer");
-var guessesLeft = document.getElementById("guesses-left");
-
-
-//start code
-document.onkeyup = function(event) {
-  var userInput = event.key;
-
-  var random = myWords[Math.floor(Math.random() * myWords.length)];
-  console.log(random);
-
-  for (var i = 0; i < random.length; i++) {
-    blankWord[i] = " - ";
-    console.log(blankWord[i]);
-  }
- 
-}
-
+var losses = 0;
+var guessesLeft = 5;

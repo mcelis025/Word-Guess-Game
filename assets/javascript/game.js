1959d36275d23459786a88722d698564ed96ba1b
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-03-14 23:29:56 -0600

    added get elements by id

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,14 @@ var myWords = ["cell", "chiaotzu", "frieza", "gohan", "goku", "krillin", "majin 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 5;
+
+// Stores what will show in index through wins-text
+var winsText = document.getElementById("wins-text");
+var currentWord = document.getElementById("current-word");
+var guessesRemaining = document.getElementById("guesses-left");
+var letterGuessed = document.getElementById("letters-guessed");
+var lossesText = document.getElementById("losses-text");
+var image = document.getElementById("picture");
+
+var random = resetGame();                                                         // Random has the function to resetGame
+var wordLength = random.length;                                                   // Saves the number of letters in random - word

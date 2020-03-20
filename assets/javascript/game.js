dfdb84e3cfd70cf2ccb8e07674ab7018efa27d42
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-03-19 23:11:14 -0600

    Completed game - added loss logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,8 +36,8 @@ function revealLetter() {
   for (var i = 0; i < random.length; i++) {
     //check if current index value is equal to 
     if (random[i] === userInput) {
-        randomPicked[i]=userInput;
-        console.log(randomPicked);
+      randomPicked[i] = userInput;
+      console.log(randomPicked);
       //update dom
       currentWord.innerHTML = randomPicked.join(" ");
     }
@@ -49,7 +49,7 @@ function reset() {
   guessesLeft = 5;
   userLetter = "";
   randomPicked = [];
-  letterGuessed.textContent = "";   
+  letterGuessed.textContent = "";
   random = myWords[Math.floor(Math.random() * myWords.length)];
   addUnderscore();
 }
@@ -60,18 +60,40 @@ document.onkeyup = function (event) {                                           
   //check if the key is valid
   if (alpha.includes(userInput)) {
     //check if the key was already pressed
-    if (!userLetter.includes(userInput)) { 
+    if (!userLetter.includes(userInput)) {
       //add that key to userLetter
-        var tText = userInput + " ";
-        letterGuessed.innerHTML += tText; 
-        userLetter += userInput;
+      var tText = userInput + " ";
+      letterGuessed.innerHTML += tText;
+      userLetter += userInput;
       //check if the word includes that key
       if (random.includes(userInput)) {
         //do the rest of your work here
         revealLetter();
         if (!randomPicked.includes("_")) {
           wins++;
-          image.src = "assets/images/" + random + ".jpg";   
+          image.src = "assets/images/" + random + ".jpg";
           winsText.textContent = "Wins: " + wins;
           reset();
         }
+        //else decrement guesses and update DOM
+      } else {
+        guessesLeft--;
+        guessesRemaining.textContent = "Wrong Guesses Left: " + guessesLeft;
+      }
+      if (guessesLeft === 0) { //is guesses = 0
+        //i lose
+        losses++;
+        image.src = "assets/images/gameOver.jpg";
+        lossesText.textContent = "Losses: " + losses;
+        reset();
+      }
+      //else alert user
+    } else {
+      alert("Letter Already Chosen")
+    }
+    //else alert user not valid key
+  } else {
+    alert("Enter Key A-Z")
+  }
+}
+
